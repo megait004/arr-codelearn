d1c7da770823b76ec0f5d2838cbf1afcd8a4efb4
Nguyễn Nguyên Giáp <103838138+megait004@users.noreply.github.com>
2024-07-21 15:30:37 +0700

    Update 66.cpp

--- 66.cpp
+++ 66.cpp
@@ -15,4 +15,28 @@ int main(){
 	for(int i:arr){
 		cout <<i << " " ;
 	}	
+	// cách 2 : không sử dùng hàm 
+	int n;
+	cin >>n;
+	int arr[n];
+	for(int i=0;i<sizeof(arr)/sizeof(int);i++){
+		cin >> arr[i];
+	}
+     for(int i=0 ;i<n;i++){
+     	for(int j=i+1;j<n;j++){
+     	int temp; // day la bien trung gian de hoan doi gia tri
+     	if(arr[i]>arr[j]){
+     		// neu nhu arr[i] > arr[j] thi hoan doi gia tri cua arr[i] va arr[j]
+     		temp = arr[i];
+     		arr[i] = arr[j];
+     		arr[j] = temp;
+		                }
+		                  }
+	                           }	
+	 for(int i=0; i<n;i++){
+	 	cout << arr[i] << " ";
+	 }
+	 return 0;
+}
+	
 }
